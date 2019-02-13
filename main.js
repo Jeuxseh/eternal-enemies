82dc22201b2a39655a89d78054745d1c55948f34
Jesús <jeusxe@hotmail.com>
2019-02-13 14:05:27 +0100

    buildsplash Dom

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 'use strict';
+const main = ()=>{
+    const buildDom=(html)=>{
+        const main = document.querySelector('main');
+        main.innerHTML = html;
+        return main;
+    }
+    const buildSplashScreen=()=>{
+
+        const splashScreen=buildDom(`
+        <section class="splash-screen">
+        <h1>Hola</h1>
+        <button>Start</button>
+        </section>
+        
+        `);
+
+    }
+    buildSplashScreen();
+}
+window.addEventListener('load',main);
